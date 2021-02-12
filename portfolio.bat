3d9c9f41cc5c18b19ffbfad101a98301d562396e
isakfinnoy <47940176+isakfinnoy@users.noreply.github.com>
2021-02-12 08:59:25 +0100

    Add files via upload

---
+++ portfolio.bat
@@ -0,0 +1,3 @@
+call C:\Users\Bruker\anaconda3\Scripts\activate.bat C:\Users\Bruker\anaconda3 
+call C:\Users\Bruker\anaconda3\python.exe "C:\Users\Bruker\OneDrive\Documents\finance_python\portfolio.py"
+cmd /k
